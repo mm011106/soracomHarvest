b51598ae876d0d91a94a981076172b451718f1cc
Masakazu Miyamoto <mm011106@gmail.com>
2018-12-17 14:56:36 +0900

    new script including BME280Driver

---
+++ soracom_send_BME280.py
@@ -0,0 +1,56 @@
+# -*- coding:utf-8 -*-
+import socket
+from contextlib import closing
+
+import os
+import commands
+import time
+
+
+from smbus import SMBus
+
+bus_number  = 1
+i2c_address = 0x76
+
+bus = SMBus(bus_number)
+
+digT = []
+digP = []
+digH = []
+
+
+def soraSend(hostName,portNumber,payload):
+    soracom = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    with closing(soracom):    # サーバを指定
+        soracom.connect((hostName, portNumber))
+    # サーバにメッセージを送る
+        soracom.sendall(payload)
+    # ネットワークのバッファサイズは1024。サーバからの文字列を取得する
+        ret=soracom.recv(1024)
+    return ret
+    #print(soracom.recv(1024))
+
+
+
+
+
+hostName='harvest.soracom.io'
+portNumber=8514
+
+interval = 18
+while True:
+    temp = commands.getoutput("vcgencmd measure_temp").split('=')[1].split('\'')[0]
+
+    payload =     "{" + "\"level\": 50.3"          + ", "
+    payload = payload + "\"contPressure\": 500"    + ", "
+    payload = payload + "\"status\": \"0xF\""          + ", "
+    payload = payload + "\"temp\":" + temp         + ", "
+    payload = payload + "\"humid\": 30.1"          + ", "
+    payload = payload + "\"atmPressure\": 1024 "
+    payload = payload + "}"
+    print time.time(), payload
+
+    print soraSend(hostName,portNumber,payload)
+
+
+    time.sleep(interval)
