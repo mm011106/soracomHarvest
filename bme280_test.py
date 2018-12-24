6f29606ff56169242f0d7bb1f604ad44d929b050
mm011106 <mm011106@gmail.com>
2018-12-24 19:48:06 +0900

    update

--- bme280_test.py
+++ bme280_test.py
@@ -1,20 +1,20 @@
 #coding: utf-8
 
-from smbus import SMBus
-import time
-
-import bme280
-
-bus_number  = 1
-i2c_address = 0x76
-
-bus = SMBus(bus_number)
-
-digT = []
-digP = []
-digH = []
-
-t_fine = 0.0
+# from smbus import SMBus
+# import time
+#
+# import bme280
+#
+# bus_number  = 1
+# i2c_address = 0x76
+#
+# bus = SMBus(bus_number)
+#
+# digT = []
+# digP = []
+# digH = []
+#
+# t_fine = 0.0
 
 bme280.setup()
 bme280.get_calib_param()
