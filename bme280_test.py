f23c38e8f9f4a2367032bed6e589d1a92c855428
mm011106 <mm011106@gmail.com>
2018-12-24 19:44:43 +0900

    test

---
+++ bme280_test.py
@@ -0,0 +1,27 @@
+#coding: utf-8
+
+from smbus2 import SMBus
+import time
+
+import bme280
+
+bus_number  = 1
+i2c_address = 0x76
+
+bus = SMBus(bus_number)
+
+digT = []
+digP = []
+digH = []
+
+t_fine = 0.0
+
+bme280.setup()
+bme280.get_calib_param()
+
+
+if __name__ == '__main__':
+	try:
+		readData()
+	except KeyboardInterrupt:
+		pass
